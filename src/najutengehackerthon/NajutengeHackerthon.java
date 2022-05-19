c1e3765c3e13e18d2bf3fad1ec6b08f6e2c9ae32
emmanuel-jan <emmanueljan80@gmail.com>
2022-05-19 19:57:55 +0300

    question1

--- src/najutengehackerthon/NajutengeHackerthon.java
+++ src/najutengehackerthon/NajutengeHackerthon.java
@@ -15,30 +15,26 @@ public class NajutengeHackerthon {
            0x00, 0x00, 0x00, 0x00
        };
        
-        int s1 = bytes[0] & 0xff;
-        int s2 = bytes[1] & 0xff;
-        int s3 = bytes[2] & 0xff;
-        int s4 = bytes[3] & 0xff;
+       //index 0 and 1 - 16 bits
+        int sourcePort = 0;
+        sourcePort |= (bytes[0] &0xff) <<8;
+        sourcePort |= (bytes[1] &0xff);
+        System.out.println("sourcePort --> "+sourcePort );
         
-        String sourcePort = s1+"."+s2+"."+s3+"."+s4;
-        System.out.println("sourcePort: " + sourcePort);
-        
-        int d1 = bytes[4] & 0xff;
-        int d2 = bytes[5] & 0xff;
-        int d3 = bytes[6] & 0xff;
-        int d4 = bytes[7] & 0xff;
-        
-        String destinationPort = d1+"."+d2+"."+d3+"."+d4;
-        System.out.println("destinationPort: " + destinationPort);
+         //index 2 and 3 - 16 bits
+        int destinationPort = 0;
+        destinationPort |= (bytes[2] &0xff) <<8;
+        destinationPort |= (bytes[3] &0xff);
+        System.out.println("destinationPort --> "+destinationPort );
         
         int vt = 0x00000000;
-        vt |= ((bytes[8]&0xff) << 24);
-        vt |= ((bytes[9]&0xff) << 16);
-        vt |= ((bytes[10]&0xff) << 8);
-        vt |= ((bytes[11]&0xff) << 0);
+        vt |= ((bytes[4]&0xff) << 24);
+        vt |= ((bytes[5]&0xff) << 16);
+        vt |= ((bytes[6]&0xff) << 8);
+        vt |= ((bytes[7]&0xff) << 0);
         
-        long vtLong = Integer.toUnsignedLong(vt);
-        System.out.println("vtLong: " + vt);
+        System.out.println("vt: " + vt);
+        
         
     }    
 }
